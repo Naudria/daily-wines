c66aac07817e62889c688bf397dbd0a80937dff0
Rima Jean <rima.jean@gmail.com>
2018-10-09 14:14:55 +0000

    first commit

---
+++ lib/daily/wines.rb
@@ -0,0 +1,7 @@
+require "daily/wines/version"
+
+module Daily
+  module Wines
+    # Your code goes here...
+  end
+end
---
+++ lib/daily/wines/version.rb
@@ -0,0 +1,5 @@
+module Daily
+  module Wines
+    VERSION = "0.1.0"
+  end
+end
